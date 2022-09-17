e63864df8c6c68f81fface022e2ddc28f14457b5
bouledepate <promtheus815@gmail.com>
2022-09-17 19:39:37 +0600

    fix dotenv

--- web/index.php
+++ web/index.php
@@ -6,8 +6,11 @@
 require realpath(dirname(__DIR__) . '/vendor/autoload.php');
 require realpath(dirname(__DIR__) . '/vendor/yiisoft/yii2/Yii.php');
 
-$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
-$dotenv->load();
+try {
+    $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+    $dotenv->load();
+} catch (\Dotenv\Exception\InvalidPathException $exception) {
+}
 
 $config = require realpath(dirname(__DIR__) . '/config/main.php');
 (new yii\web\Application($config))->run();
